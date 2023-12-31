0f977d10a884f2ce4d84b5532aaf95eac7c994e4
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2023-12-31 15:16:59 -0300

    encapsulando o server.ts

--- src/database/connect.database.ts
+++ src/database/connect.database.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import mongoose from 'mongoose';
 
-const uri = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}?authSource=admin`;
+const uri = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}?authSource=admin`;
 
 
 const connecDataBase = async () => {
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import 'dotenv/config';
 import { connecDataBase } from './database/connect.database';
-import app from "./app";
+import app from './app';
 
-const port = process.env.PORT || 3000;
+const startServer = async () => {
+    try {
+        await connecDataBase();
+        const port = process.env.PORT || 3000;
+        app.listen(port, () => console.log(`Server is running on port ${port}`));
+    } catch (error) {
+        console.error('Error starting the server:', error);
+    }
+};
 
-connecDataBase();
-
-app.listen(port, () => console.log(`Server is running port ${port}`));
+startServer();
