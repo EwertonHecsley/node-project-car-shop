c22cf4ad7d7b0f160812feac4ae3b8c27a642f63
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2023-12-31 15:03:44 -0300

    commit inicial, estrutura base e conexao com banco de dados mongo

---
+++ src/database/connect.database.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import mongoose from 'mongoose';
+
+const uri = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}?authSource=admin`;
+
+
+const connecDataBase = async () => {
+    try {
+        await mongoose.connect(uri);
+        console.log(`Database connected`);
+    } catch (error) {
+        console.error(`Error to connect database`, error);
+    }
+};
+
+export { connecDataBase }
