ce3d53249653760451fff1a6ea627464ac45c72c
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2023-12-31 15:05:32 -0300

    adicionando conexao no app

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import 'dotenv/config';
+import { connecDataBase } from './database/connect.database';
 import app from "./app";
 
 const port = process.env.PORT || 3000;
+
+connecDataBase();
 
 app.listen(port, () => console.log(`Server is running port ${port}`));
